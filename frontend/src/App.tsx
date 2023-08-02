6b271c8a8d5a56c43df3e010fef465a19147e973
EduardoDallarosa <eduardodallarosa321@gmail.com>
2023-08-01 22:15:35 -0300

    Aula 01/08/2023

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { Divider } from "@mui/material"
+import TopicCard from "./components/TopicCard"
+
 function App() {
 
   return (
     <div id="App">
-      
+      <TopicCard/>
+      <Divider/>
+      <TopicCard/>
     </div>
   )
 }
---
+++ frontend/src/components/TopicCardBody/index.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+function TopicCardBody() {
+    return (
+        <div id="topic-card-body" style={{marginLeft: '3rem'}}>
+            <Typography variant="body1">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam praesentium aliquid assumenda voluptatum aspernatur nostrum ad quaerat. Tempora culpa, labore molestiae, tempore distinctio asperiores cumque odit aliquam similique velit cum.
+            </Typography>
+        </div>
+    )
+}
+
+export default TopicCardBody;

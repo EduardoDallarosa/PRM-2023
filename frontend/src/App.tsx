4045b85b4bc6e2d3a21965983fcf44817761808c
EduardoDallarosa <eduardodallarosa321@gmail.com>
2023-08-15 17:45:58 -0300

    Aula 14/08/2023

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
-import { Divider } from "@mui/material"
-import TopicCard from "./components/TopicCard"
 
 function App() {
-
+  
   return (
     <div id="App">
-      <TopicCard/>
-      <Divider/>
-      <TopicCard/>
+      
     </div>
   )
 }

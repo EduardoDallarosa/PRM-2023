93ff82ab9ccaeeab2e98df80b84dbaeaf3be6735
EduardoDallarosa <eduardodallarosa321@gmail.com>
2023-10-24 21:11:16 -0300

    Aula 24/10/2023

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,9 +6,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProviver } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <AuthContextProviver>
     <App />
-  </React.StrictMode>
+    </AuthContextProviver>
+  </BrowserRouter>
 )
